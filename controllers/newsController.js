ea5db83053615652a3cb00d19f5e7c76152f7a18
ab_normal <abrhamteshe321@gmail.com>
2026-04-27 10:56:59 +0300

    WIP: personalized news

--- controllers/newsController.js
+++ controllers/newsController.js
@@ -1,13 +1,16 @@
 const axios = require("axios");
 
 const getNews = async (req, res) => {
+
+  const watches = req.watches ? req.watches : ["TSLA", "CAMZN", "CMSFT"]
   try {
     const response = await axios.get(
-      `https://api.marketaux.com/v1/news/all?symbols=TSLA%2CAMZN%2CMSFT&filter_entities=true&language=en&api_token=${process.env.NEWS_API_KEY}`,
+      `https://api.marketaux.com/v1/news/all?symbols=${watches.join('%2')}&filter_entities=true&language=en&api_token=${process.env.NEWS_API_KEY}`,
     );
 
     res.status(200).json({ data: response.data });
   } catch (error) {
+    console.log(error)
     res.status(400).json({ error: "could not fetch news." });
   }
 };
